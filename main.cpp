08c1c95f1f1428fbcfe7a0d7a62f8c7964a68721
hashub <1055757507@qq.com>
2025-08-05 23:02:44 +0800

    feat: Update CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <SDL.h>
 
-int main()
+int main(int, char**)
 {
   int width = 800;
   printf("width: %d\n", width);
   std::cout << "Hello, World!" << std::endl;
   return 0;
 }
-
