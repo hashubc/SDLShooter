0895c10f7f9203efe0f49f164dd0ad14117af931
hashub <1055757507@qq.com>
2025-08-05 23:28:37 +0800

    feat: Add SDL framework

--- main.cpp
+++ main.cpp
@@ -3,8 +3,39 @@
 
 int main(int, char**)
 {
-  int width = 800;
-  printf("width: %d\n", width);
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+    // SDL initialization
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow("SDL Shooter", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+
+    // Create a renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    // Do something with the renderer
+    while (true) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                break;
+            }
+        }
+
+        // Clear the screen
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // Present the renderer
+        SDL_RenderPresent(renderer);
+    }
+
+    // clean up
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
