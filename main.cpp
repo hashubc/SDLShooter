5f836d181246c2e845af0beb33dedcccb7546380
hashub <1055757507@qq.com>
2025-08-05 22:58:04 +0800

    feat: Update CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <SDL.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
+  int width = 800;
+  printf("width: %d\n", width);
   std::cout << "Hello, World!" << std::endl;
   return 0;
 }
