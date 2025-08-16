b6776ad470c173e5fe8a8db977ff919d582a7da1
hashub <1055757507@qq.com>
2025-08-16 17:46:09 +0800

    completed tutorial

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
 
 int main(int, char**)
 {
@@ -15,22 +18,80 @@ int main(int, char**)
     // Create a renderer
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
+    // SDL_image initialization
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        std::cerr << "SDL_image could not initialize! IMG_Error: " << IMG_GetError() << std::endl;
+        return 1;
+    }
+    // Load an image (example)
+    SDL_Texture *texture = IMG_LoadTexture(renderer, "assets/image/bg.png");
+
+    // SDL_Mixer初始化
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
+        std::cerr << "Mix_OpenAudio Error: " << Mix_GetError() << std::endl;
+        return 1;
+    }
+    // 读取音乐
+    Mix_Music *music = Mix_LoadMUS("assets/music/03_Racing_Through_Asteroids_Loop.ogg");
+    // 播放音乐
+    Mix_PlayMusic(music, -1);
+
+
+    // SDL_ttf初始化
+    if (TTF_Init() == -1) {
+        std::cerr << "TTF_Init Error: " << TTF_GetError() << std::endl;
+        return 1;
+    }
+    // 加载字体
+    TTF_Font *font = TTF_OpenFont("assets/font/VonwaonBitmap-16px.ttf", 24);
+    // 创建文本纹理
+    SDL_Color color = {255, 255, 255, 255};
+    SDL_Surface *surface = TTF_RenderUTF8_Solid(font, "Hello, SDL! 中文也可以", color);
+    SDL_Texture *textTexture = SDL_CreateTextureFromSurface(renderer, surface);
+
+
+
     // Do something with the renderer
-    while (true) {
+    bool running = true;
+    while (running) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
-                break;
+                running = false;
             }
         }
-
-        // Clear the screen
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        // Present the renderer
+        // 画一个长方形
+        SDL_Rect rect = {100, 100, 200, 200};
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &rect);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+        // 画图片
+        SDL_Rect dstrect = {200, 200, 200, 200};
+        SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+
+        // 画文本
+        SDL_Rect textRect = {300, 300, surface->w, surface->h};
+        SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
+
+        // 更新屏幕
         SDL_RenderPresent(renderer);
     }
+    SDL_DestroyTexture(texture);
+    IMG_Quit();
+
+    // 清理音乐资源
+    Mix_FreeMusic(music);
+    Mix_CloseAudio();
+    Mix_Quit();
+
+    // 清理字体资源
+    SDL_FreeSurface(surface);
+    SDL_DestroyTexture(textTexture);
+    TTF_CloseFont(font);
+    TTF_Quit();
 
     // clean up
     SDL_DestroyRenderer(renderer);
